cfa23ef79d5d89485d607e256b6b22e71495d6fc
aubreyshere <aubreyh156@gmail.com>
2024-12-16 15:50:44 -0800

    Changed main entry point to index.js from App.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const navigationBar = (
+    <div className='nav'>
+        <button className='homepage'>Homepage</button>
+        <button className='createEvent'>Create Event</button>
+        <button className='profile'>Profile</button>
+        mrow
+    </div>
 );
+root.render(navigationBar);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
